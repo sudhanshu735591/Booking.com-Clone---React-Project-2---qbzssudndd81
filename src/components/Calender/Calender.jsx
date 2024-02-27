a4e951b8d69708fc65e3afd1841cefde43d986d7
Sudhanshu <ersshekhar05@gmail.com>
2024-02-27 16:16:41 +0530

    updated code push

---
+++ src/components/Calender/Calender.jsx
@@ -0,0 +1,32 @@
+import { addDays } from "date-fns";
+import { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import "./Calender.css"
+
+function CalenderApp() {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection"
+    }
+  ]);
+
+  return (
+    <div>
+      <DateRangePicker
+        onChange={(item) => setState([item.selection])}
+        showSelectionPreview={true}
+        moveRangeOnFirstSelection={false}
+        months={2}
+        ranges={state}
+        direction="horizontal"
+      />
+    </div>
+  );
+}
+
+
+export default CalenderApp;
